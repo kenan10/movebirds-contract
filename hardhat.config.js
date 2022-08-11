9fbfe9ec3c6fc9e93dcc3e36e4cc092925ddbaf9
kenan10 <denis2014grom@gmail.com>
2022-08-11 16:26:28 +0300

    add configuration for mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,14 +9,29 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
+const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY
 
 module.exports = {
-    solidity: '0.8.9',
+    solidity: {
+        version: '0.8.9',
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
+    },
     defaultNetwork: 'hardhat',
     networks: {
         hardhat: {
             chainId: 31337,
             gasPrice: 33000000000
+        },
+        mainnet: {
+            chainId: 1,
+            url: MAINNET_RPC_URL,
+            accounts: [MAINNET_PRIVATE_KEY]
         },
         rinkeby: {
             chainId: 4,
